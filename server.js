f1ddc160f4db6e6de1657d672a0fdf62121000c1
Aashish Kapur <me@aashishkapur.com>
2015-11-27 23:31:36 -0800

    more changes, almost there

--- server.js
+++ server.js
@@ -21,6 +21,28 @@ app.post('/', function(req, res){
 	console.log("REQ: " + req.body.email);
 
 	res.end();
+	var auth = 'Basic ' + new Buffer('username:' + apiKey).toString('base64');
+	var url = "https://us12.api.mailchimp.com/3.0/lists/" + listID + "/members";
+	var data = JSON.stringify({email_address : req.body.email, status : "subscribed"});
+	request.post(
+		
+			"https://us12.api.mailchimp.com/3.0/lists/" + listID + "/members",
+		
+		{
+			json : true,
+			body : data,
+		
+		
+			auth :{
+				'user': 'anystring',
+				'pass': apiKey
+			}
+		},
+		function (error, response, body) {
+			console.log('error: '+ response.statusCode);
+			console.log(body);
+		}
+	);
 
 
 
